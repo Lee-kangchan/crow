8ff5b643af3e6a7f545f439a6068fe2f392467df
lee-kangchan <lsc4237@gmail.com>
2022-10-12 17:21:34 +0900

    default

--- src/main/java/com/dev/crow/CrowApplication.java
+++ src/main/java/com/dev/crow/CrowApplication.java
@@ -8,6 +8,7 @@ public class CrowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrowApplication.class, args);
+
     }
 
 }
--- src/main/java/com/dev/crow/member/domain/Member.java
+++ src/main/java/com/dev/crow/member/domain/Member.java
@@ -1,7 +1,32 @@
-package com.dev.crow.member.domain;/** 
- * @FileName :    Member 
- * @Date :        2022/10/12 
+package com.dev.crow.member.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
  * @author :      lee_kangchan
+ * @FileName :    Member
+ * @Date :        2022/10/12
  * @Discription : 멤버 정보
- */public class Member {
+ */
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "seq")
+    private Long seq;
+
+    private String email;
+    private String password;
+    private String name;
+
 }
