725de98f8ffbd406226345d7c7a4bbc19f7bcd2c
leekangchan <kangchan.lee@futuredigm.co.kr>
2022-10-12 17:08:41 +0900

    Initial commit

---
+++ src/main/java/com/dev/crow/member/domain/Member.java
@@ -0,0 +1,7 @@
+package com.dev.crow.member.domain;/** 
+ * @FileName :    Member 
+ * @Date :        2022/10/12 
+ * @author :      lee_kangchan
+ * @Discription : 멤버 정보
+ */public class Member {
+}
